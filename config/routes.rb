4948011021a3070bb1882e25b2cdb01d61b71a8d
Ryan Hageman <improv42@gmail.com>
2016-08-17 20:44:51 -0500

    Set up CRUD routes for tweets and root

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,11 @@ Rails.application.routes.draw do
 
   get 'tweets/_form'
 
+  resources :tweets
+
   devise_for :users
+
+  root 'tweets#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
