0c6ae8dd1e2504b8fe33a02400e3abc9a5c44a65
Ryan Hageman <improv42@gmail.com>
2016-08-24 20:33:19 -0500

    Set up unlike

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,6 +16,10 @@ class User < ActiveRecord::Base
   has_many :likes
 
   validates :username, presence: true, uniqueness: true
+
+  def likes?(tweet)
+    tweet.likes.where(user_id: id).any?
+  end
 end
 
 
